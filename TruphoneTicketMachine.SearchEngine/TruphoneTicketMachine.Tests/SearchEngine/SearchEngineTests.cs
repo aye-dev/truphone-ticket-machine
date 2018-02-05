8aae70ce62349187f921e02748ad1677c5c395a7
Alexander Yefimochkin <alexander.yefimochkin@gmail.com>
2018-02-05 20:58:00 +0000

    Initial setup, search engine brainstorming

---
+++ TruphoneTicketMachine.SearchEngine/TruphoneTicketMachine.Tests/SearchEngine/SearchEngineTests.cs
@@ -0,0 +1,20 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+
+namespace TruphoneTicketMachine.Tests.SearchEngine
+{
+    // Brainstorming
+    // -> Given a search term, search engine should retrieve the possible destinations 
+    // and next letters matching the search term
+    // -> It should rely on some kind of string search algorithm - search effience is a key
+    // -> It should rely on some kind of data source and be able to load the data
+    // -> It should use the data source to load the data, search the "searchTerm" using the algorithm and
+    // retrieve the corresponding destinations and next character suggestions to be presented to the user
+
+    [TestFixture]
+    public class SearchEngineTests
+    {
+
+    }
+}
